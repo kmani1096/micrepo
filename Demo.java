e0bda003d9f34528ff6f2b4f3b811b07a23a6ed8
chandram6 <x21117284@student.ncirl.ie>
2023-05-01 15:32:59 +0100

    local

--- Demo.java
+++ Demo.java
@@ -1,3 +1,5 @@
+import javax.xml.transform.stream.StreamSource;
+
 public class Demo {
     int x = 4;
 
@@ -8,6 +10,7 @@ public class Demo {
         System.out.println(myObj2.x);
         System.out.println(myObj2.x);
         System.out.println(myObj2.l);
+        System.out.println("manisha");
     }
 
 }

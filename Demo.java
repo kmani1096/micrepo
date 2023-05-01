37538baa2321bee01e0db9cb4e9d7529b2de3ad7
chandram6 <x21117284@student.ncirl.ie>
2023-05-01 20:51:54 +0100

    Revert "revoke-2"
    This reverts commit a7d09cbf

--- Demo.java
+++ Demo.java
@@ -4,11 +4,9 @@ public class Demo {
     int x = 4;
 
     public static void main(String[] args) {
-        System.out.println("lalkh");
-        System.out.println("hjhvjm");
+        System.out.println("lali");
+        System.out.println("hfh");
         System.out.println("demo");
-        System.out.println("jhbj");
-
     }
 
 }

c92462d6dc135eeaebf1a5cf5797513a42b8a736
chandram6 <x21117284@student.ncirl.ie>
2023-05-01 15:25:19 +0100

    main 2

--- Demo.java
+++ Demo.java
@@ -4,7 +4,7 @@ public class Demo {
     public static void main(String[] args) {
         Main myObj1 = new Main();  // Object 1
         Main myObj2 = new Main();  // Object 2
-        System.out.println(myObj1.x);
+        System.out.println(myObj1.y);
         System.out.println(myObj2.x);
     }
 

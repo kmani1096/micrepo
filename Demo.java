684f77ad524f046f3c1001eb6912f685bf73f8f2
chandram6 <x21117284@student.ncirl.ie>
2023-05-01 20:51:00 +0100

    revoke-3

--- Demo.java
+++ Demo.java
@@ -4,7 +4,7 @@ public class Demo {
     int x = 4;
 
     public static void main(String[] args) {
-        System.out.println("lali");
+        System.out.println("lalkh");
         System.out.println("hjhvjm");
         System.out.println("demo");
         System.out.println("jhbj");
